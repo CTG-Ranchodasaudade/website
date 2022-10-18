fda39b55750efaeb3ec1c0c6e85ee4753e4dba1b
CTG-Ranchodasaudade <ranchodasaudade.ctg@gmail.com>
2022-10-18 17:25:49 -0300

    Organização
    organização das imagens e teste do script de animação do slide

--- scripts/script_main.js
+++ scripts/script_main.js
@@ -10,12 +10,18 @@
     
     const slide= [img1,img2,img3,img4,img5,img6];
     
-    for(let num=1; num > 10; num++ ){
+    for(let num=0; num < slide.length; num++ ){
         console.log(slide[num]);
-        if(num == slide.length){
+        let num2 = num;
+        slide[num].style.display = 'block';
+        slide[num2].style.display = 'none';
+        
+        if(slide[num] == (slide.length)){
             num=0;
         }
-        console.log(num);
+        
+        
     }
-    console.log('function funcionando')
+    
+    
 })();
