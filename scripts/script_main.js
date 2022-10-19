11f2f9049aa6f4e08396367686aea23802171763
CTG-Ranchodasaudade <ranchodasaudade.ctg@gmail.com>
2022-10-19 11:06:24 -0300

    img_or
    Devido a problemas de leitura de imagens , adicionamos outro subnivel

--- scripts/script_main.js
+++ scripts/script_main.js
@@ -3,7 +3,7 @@
     let countImg = 0;
     const containerExt = document.getElementById("slide");
     const arrayFoto = ['34_cavalgada_do_mar', 'Busca_da_chama_crioula', 'cerimonia_de_premiacao_em_cachoeirinha(2)','Cerimonia_de_premiacao_em_cachoeirinha','ENART_em_santa_cruz_do_sul','Image[4]'];
-    containerExt.style.backgroundImage=`url('../img/img_slide/34_cavalgada_do_mar.jpg')`;
+    containerExt.style.backgroundImage=`url('../../img/img_slide/34_cavalgada_do_mar.jpg')`;
 
     setInterval(function () {
         countImg++
@@ -12,7 +12,7 @@
             countImg = 0;
         }
         mudaFoto = arrayFoto[countImg]
-        containerExt.style.backgroundImage=`url('../img/img_slide/${mudaFoto}.jpg')`;
+        containerExt.style.backgroundImage=`url('../../img/img_slide/${mudaFoto}.jpg')`;
         
     },5000);
    
