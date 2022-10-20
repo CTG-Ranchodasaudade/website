fbdc6f9f2abf605074a3f2c7d4827305927f04cc
CTG-Ranchodasaudade <ranchodasaudade.ctg@gmail.com>
2022-10-20 14:17:56 -0300

    commit

--- scripts/script_main.js
+++ scripts/script_main.js
@@ -2,13 +2,13 @@
 (function sliderMovel() {
     let countImg = 0;
     const containerExt = document.getElementById("slide");
-    const arrayFoto = ['img1','34_cavalgada_do_mar', 'Busca_da_chama_crioula', 'cerimonia_de_premiacao_em_cachoeirinha(2)','Cerimonia_de_premiacao_em_cachoeirinha','ENART_em_santa_cruz_do_sul','Image[4]'];
+    const arrayFoto = ['34_cavalgada_do_mar', 'Busca_da_chama_crioula', 'cerimonia_de_premiacao_em_cachoeirinha(2)','Cerimonia_de_premiacao_em_cachoeirinha','ENART_em_santa_cruz_do_sul','Image[4]'];
     containerExt.style.backgroundImage=`url('/website/../img/img_slide/34_cavalgada_do_mar.jpg')`;
 
     setInterval(function () {
         countImg++
         let mudaFoto = 0;
-        if(countImg == 7) {
+        if(countImg == 6) {
             countImg = 0;
         }
         mudaFoto = arrayFoto[countImg]
